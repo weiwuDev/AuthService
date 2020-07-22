7b40bd2874320ce95ed83e5787944e56425cf481
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-22 17:19:19 -0400

    test

--- src/main/java/com/weiwudev/controllers/RegistrationController.java
+++ src/main/java/com/weiwudev/controllers/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class RegistrationController {
 
