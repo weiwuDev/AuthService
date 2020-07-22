50167083618874e63369406d7742ac75ae9f6e76
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-22 12:38:14 -0400

    first

---
+++ src/main/java/com/weiwudev/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package com.weiwudev.controllers;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/login")
+    public Mono<String> login(WebSession webSession) {
+        return Mono.just("Login Successful");
+    }
+
+    @PutMapping("/logout")
+    public Mono<String> logout(WebSession webSession) {
+        webSession.invalidate();
+        return Mono.just("Logout Successful");
+    }
+
+}
