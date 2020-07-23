f88b0072a40bb3bfd46888055eb0d03d7f3ae00a
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-22 21:21:16 -0400

    test

--- src/main/java/com/weiwudev/controllers/AuthController.java
+++ src/main/java/com/weiwudev/controllers/AuthController.java
@@ -4,7 +4,6 @@ package com.weiwudev.controllers;
 import com.weiwudev.models.ResponseObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +14,16 @@ import reactor.core.publisher.Mono;
 @RestController
 public class AuthController {
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public Mono<ResponseEntity<ResponseObject>> login(WebSession webSession) {
-        return Mono.just(ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("Login Successful", webSession.getId())));
+        return Mono.just(ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("Login Successful", "SESSION=" + webSession.getId())));
 
     }
 
     @PutMapping("/logout")
     public Mono<ResponseEntity<ResponseObject>> logout(WebSession webSession) {
         webSession.invalidate();
-        return Mono.just(ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("Login Successful", webSession.getId())));
+        return Mono.just(ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("Login Successful", "SESSION=" + webSession.getId())));
     }
 
 }
