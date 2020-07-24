6aba0e3a6171fe7909a121e9befe5bb0a7e32600
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-23 22:13:37 -0400

    testing config updates

--- src/main/java/com/weiwudev/config/SecurityConfig.java
+++ src/main/java/com/weiwudev/config/SecurityConfig.java
@@ -1,20 +1,19 @@
 package com.weiwudev.config;
 
-import com.weiwudev.encoders.CustomPasswordEncoder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 
-
-@EnableWebFluxSecurity
 @Configuration
+@EnableWebFluxSecurity
+@RefreshScope
 public class SecurityConfig {
 
     private final ServerSecurityContextRepository securityContextRepository = new WebSessionServerSecurityContextRepository();
